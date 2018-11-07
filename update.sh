1117be14a93ff7a52ad21ed507c193c608222b1b
Maurits van der Schee <maurits@vdschee.nl>
2018-11-07 15:18:33 +0100

    add update script

---
+++ update.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+VERSION=$(curl -s https://github.com/mintyphp/mintyphp/releases | grep -iA 10 'label-latest' | grep -o '"v[0-9]\+\.[0-9]\+\.[0-9]\+"')
+VERSION="${VERSION//\"}" # strip quote character
+VERSION="${VERSION//v}" # strip v character
+OLDVER=$(cat version.txt)
+echo $VERSION > version.txt
+sed -i "s/$OLDVER/$VERSION/g" 01-installation.md
+git commit -am "update latest" && git push
